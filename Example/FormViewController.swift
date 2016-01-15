cc37ff74e686653d9764835d45eed55af1550df2
Aaron McTavish <aamct@ustwo.com>
2016-01-15 11:38:34 +0000

    Added `ControlForm` example and expanded UI tests

--- Example/FormViewController.swift
+++ Example/FormViewController.swift
@@ -17,6 +17,8 @@ final class FormViewController: UIViewController {
     
     // MARK: - Properties
     
+    var form = ControlForm()
+    
     private var underlyingView: FormView {
         if let myView = view as? FormView {
             return myView
@@ -32,6 +34,38 @@ final class FormViewController: UIViewController {
     
     override func loadView() {
         view = FormView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form.addControl(underlyingView.nameEntry.textField)
+        form.addControl(underlyingView.emailEntry.textField)
+        
+        underlyingView.submitButton.addTarget(self, action: Selector("submitButtonPressed:"), forControlEvents: .TouchUpInside)
+    }
+    
+    
+    // MARK: - Control Actions
+    
+    func submitButtonPressed(sender: UIButton) {
+        let alertTitle: String
+        let alertMessage: String
+        if form.isValid {
+            alertTitle = NSLocalizedString("Success", comment: "")
+            alertMessage = NSLocalizedString("Your data has been submitted!", comment: "")
+        } else {
+            alertTitle = NSLocalizedString("Error", comment: "")
+            alertMessage = NSLocalizedString("Please correct your entries in the form.", comment: "")
+        }
+        
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .Alert)
+        alertController.popoverPresentationController?.sourceView = sender
+        
+        let doneAction = UIAlertAction(title: NSLocalizedString("Done", comment: ""), style: .Default, handler: nil)
+        alertController.addAction(doneAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
 }
--- Tests/UI Tests/FormTests.swift
+++ Tests/UI Tests/FormTests.swift
@@ -24,17 +24,80 @@ class FormTests: XCTestCase {
         super.tearDown()
     }
     
-    func testNameTextField_NoAllowedViolations() {
+    func testTextField_NoAllowedViolations() {
         let app = XCUIApplication()
         let expectedResult = "Foo"
         
-        let nameTextfieldTextField = app.textFields[FormAccessibility.Identifiers.NameTextField]
-        nameTextfieldTextField.tap()
-        nameTextfieldTextField.typeText(expectedResult + "12 ?")
+        let nameTextField = app.textFields[FormAccessibility.Identifiers.NameTextField]
+        nameTextField.tap()
+        nameTextField.typeText(expectedResult + "12 ?")
         
-        let actualResult = nameTextfieldTextField.value as? String
+        let actualResult = nameTextField.value as? String
         
         XCTAssertEqual(actualResult, expectedResult, "The text field should have text \(expectedResult) but received \(actualResult).")
+    }
+    
+    func testTextField_AllowedViolations() {
+        let app = XCUIApplication()
+        let expectedResult = "Foo12 ?"
+        
+        let emailTextField = app.textFields[FormAccessibility.Identifiers.EmailTextField]
+        emailTextField.tap()
+        emailTextField.typeText(expectedResult)
+        
+        let actualResult = emailTextField.value as? String
+        
+        XCTAssertEqual(actualResult, expectedResult, "The text field should have text \(expectedResult) but received \(actualResult).")
+    }
+    
+    func testForm_Valid() {
+        let app = XCUIApplication()
+        
+        let name = "Foo"
+        let email = "Bar@Baz.com"
+        
+        let nameTextField = app.textFields[FormAccessibility.Identifiers.NameTextField]
+        let emailTextField = app.textFields[FormAccessibility.Identifiers.EmailTextField]
+        let submitButton = app.buttons[FormAccessibility.Identifiers.SubmitButton]
+        
+        nameTextField.tap()
+        nameTextField.typeText(name)
+        
+        emailTextField.tap()
+        emailTextField.typeText(email)
+        
+        let successAlert = app.staticTexts[NSLocalizedString("Success", comment: "")]
+        let exists = NSPredicate(format: "exists == true")
+        expectationForPredicate(exists, evaluatedWithObject: successAlert, handler: nil)
+        
+        submitButton.tap()
+        
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
+    
+    func testForm_Invalid() {
+        let app = XCUIApplication()
+        
+        let name = "Foo"
+        let email = "Bar"
+        
+        let nameTextField = app.textFields[FormAccessibility.Identifiers.NameTextField]
+        let emailTextField = app.textFields[FormAccessibility.Identifiers.EmailTextField]
+        let submitButton = app.buttons[FormAccessibility.Identifiers.SubmitButton]
+        
+        nameTextField.tap()
+        nameTextField.typeText(name)
+        
+        emailTextField.tap()
+        emailTextField.typeText(email)
+        
+        let successAlert = app.staticTexts[NSLocalizedString("Error", comment: "")]
+        let exists = NSPredicate(format: "exists == true")
+        expectationForPredicate(exists, evaluatedWithObject: successAlert, handler: nil)
+        
+        submitButton.tap()
+        
+        waitForExpectationsWithTimeout(5.0, handler: nil)
     }
     
 }
