f1479432f75d6b1245fcd6a51d70dd4de22af5e1
Onur Ersel <onurersel@gmail.com>
2016-12-07 17:05:28 +0100

    Fixed SwiftLint issues. Updated credit card regular expressions. Converted ‘CreditCardCondition‘ from enum to OptionSet. Code convention fix on ‘CreditCardCondition‘. Overhauled ‘cardTypeOf‘ function of ‘CreditCardValidator‘. DRYed ‘CreditCardValidator‘. DRYed ‘CreditCardConditionTests‘.

--- Sources/Conditions/CreditCardCondition.swift
+++ Sources/Conditions/CreditCardCondition.swift
@@ -10,45 +10,100 @@ import Foundation
 
 
 /// Credit card types that are supported by `CreditCardCondition`. There is a `regex` property that returns the regex for validating that credit card number.
-public enum CreditCardType : String {
-    case americanExpress    = "American Express"
-    case dinersClub         = "Diners Club"
-    case discover           = "Discover"
-    case jcb                = "JCB"
-    case maestro            = "Maestro"
-    case mastercard         = "Master Card"
-    case visa               = "VISA"
-    case all                = "all"
+public struct CreditCardType: OptionSet {
+    public var rawValue: Int
     
-    var regex: String {
-        switch self {
-        case .americanExpress:
-            return "3[47][0-9]{13}"
-        case .dinersClub:
-            return "3(?:0[0-5]|[68][0-9])[0-9]{11}"
-        case .discover:
-            return "6(?:011|5[0-9]{2})(?:[0-9]{12})"
-        case .jcb:
-            return "(?:2131|1800|35\\d{3})\\d{11}"
-        case .maestro:
-            return "(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15}"
-        case .mastercard:
-            return "5[1-5][0-9]{14}"
-        case .visa:
-            return "^4[0-9]{12}(?:[0-9]{3})?$"
-        case .all:
-            let allCardTypes : [CreditCardType] = [.americanExpress, .dinersClub, .discover, .jcb, .maestro, .mastercard, .visa]
-            let allCardRegexes : [String] = allCardTypes.map{ return $0.regex }
-            return allCardRegexes.joined(separator: "|")
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    static let americanExpress    = CreditCardType(rawValue: 1 << 0)
+    static let dinersClub         = CreditCardType(rawValue: 1 << 1)
+    static let discover           = CreditCardType(rawValue: 1 << 2)
+    static let jcb                = CreditCardType(rawValue: 1 << 3)
+    static let maestro            = CreditCardType(rawValue: 1 << 4)
+    static let mastercard         = CreditCardType(rawValue: 1 << 5)
+    static let visa               = CreditCardType(rawValue: 1 << 6)
+    
+    static let all: CreditCardType = [.americanExpress, .dinersClub, .discover, .jcb, .maestro, .mastercard, .visa]
+    
+    
+    
+    var description: String {
+        switch self.rawValue {
+        case CreditCardType.americanExpress.rawValue:
+            return "American Express"
+        case CreditCardType.dinersClub.rawValue:
+            return "Diners Club"
+        case CreditCardType.discover.rawValue:
+            return "Discover"
+        case CreditCardType.jcb.rawValue:
+            return "JCB"
+        case CreditCardType.maestro.rawValue:
+            return "Maestro"
+        case CreditCardType.mastercard.rawValue:
+            return "Master Card"
+        case CreditCardType.visa.rawValue:
+            return "VISA"
+        case CreditCardType.all.rawValue:
+            return "All"
+        default: return ""
         }
     }
+    
+    private var cardRegex: String? {
+        switch self {
+        case CreditCardType.americanExpress:
+            return "^3[47][0-9]{13}$"
+        case CreditCardType.dinersClub:
+            return "^3(?:0[0-5]|[68][0-9])[0-9]{11}$"
+        case CreditCardType.discover:
+            return "^6(?:011|5[0-9]{2})[0-9]{12}$"
+        case CreditCardType.jcb:
+            return "^(?:2131|1800|35\\d{3})\\d{11}$"
+        case CreditCardType.maestro:
+            return "^(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15}$"
+        case CreditCardType.mastercard:
+            return "^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$"
+        case CreditCardType.visa:
+            return "^4[0-9]{12}(?:[0-9]{3})?$"
+        default: return nil
+        }
+    }
+    
+    private func insertCard(regexString: inout String, cardType: CreditCardType) {
+        guard let cardTypeRegex = cardType.cardRegex else { return }
+        
+        if self.contains(cardType) {
+            if !regexString.isEmpty {
+                regexString.append("|")
+            }
+            
+            regexString.append(cardTypeRegex)
+        }
+    }
+    
+    var regex: String {
+        var str = ""
+        self.insertCard(regexString: &str, cardType: .americanExpress)
+        self.insertCard(regexString: &str, cardType: .dinersClub)
+        self.insertCard(regexString: &str, cardType: .discover)
+        self.insertCard(regexString: &str, cardType: .jcb)
+        self.insertCard(regexString: &str, cardType: .maestro)
+        self.insertCard(regexString: &str, cardType: .mastercard)
+        self.insertCard(regexString: &str, cardType: .visa)
+        
+        return str
+    }
 }
+
+
 
 
 /**
  *  The `CreditCardCondition` checks a string for a credit card number.
  */
-public struct CreditCardCondition : Condition {
+public struct CreditCardCondition: Condition {
     
     // MARK: - Properties
     
@@ -60,7 +115,7 @@ public struct CreditCardCondition : Condition {
     
     public var cardType: CreditCardType {
         didSet {
-            self.regex = cardType.regex
+            regex = cardType.regex
         }
     }
     
@@ -77,7 +132,7 @@ public struct CreditCardCondition : Condition {
      Initializes a `CreditCardCondition`.
      - parameter cardType: Credit card type to use for validation.
      */
-    init (cardType : CreditCardType) {
+    init (cardType: CreditCardType) {
         self.cardType = cardType
         self.regex = cardType.regex
     }
@@ -87,16 +142,15 @@ public struct CreditCardCondition : Condition {
      */
     public func check(_ text: String?) -> Bool {
         guard let sourceText = text,
-            let regExp = try? NSRegularExpression(pattern: self.regex, options: .caseInsensitive) else
-        {
+            let regExp = try? NSRegularExpression(pattern: self.regex, options: .caseInsensitive) else {
             return false
         }
         
-        let trimmedText = String(sourceText.characters.filter{ $0 != " " })
+        let trimmedText = String(sourceText.characters.filter { $0 != " " })
         return check(trimmedText, withRegex: regExp)
     }
     
     public func check(_ trimmedText: String, withRegex regExp: NSRegularExpression) -> Bool {
-        return regExp.firstMatch(in: trimmedText, options: [], range: NSMakeRange(0, trimmedText.characters.count)) != nil
+        return regExp.firstMatch(in: trimmedText, options: [], range: NSRange(location:0, length:trimmedText.characters.count)) != nil
     }
 }
--- Tests/Unit Tests/Conditions/CreditCardConditionTests.swift
+++ Tests/Unit Tests/Conditions/CreditCardConditionTests.swift
@@ -18,28 +18,23 @@ final class CreditCardConditionTests: XCTestCase {
     
     // Conditions
     let conditionAll                = CreditCardCondition()
-    let conditionAmericanExpress    = CreditCardCondition(cardType: .americanExpress)
-    let conditionDinersClub         = CreditCardCondition(cardType: .dinersClub)
-    let conditionDiscover           = CreditCardCondition(cardType: .discover)
-    let conditionJcb                = CreditCardCondition(cardType: .jcb)
-    let conditionMaestro            = CreditCardCondition(cardType: .maestro)
-    let conditionMasterCard         = CreditCardCondition(cardType: .mastercard)
-    let conditionVisa               = CreditCardCondition(cardType: .visa)
-    
-    // Valid Card Numbers
-    let cardNumberAmericanExpress   = "376031710126369"
-    let cardNumberDinersClub        = "30085182354725"
-    let cardNumberDiscover          = "6011359046468736"
-    let cardNumberJcb               = "3535983484092395"
-    let cardNumberMaestro           = "6762856858323942"
-    let cardNumberMastercard        = "5480785928215247"
-    let cardNumberVisa              = "4024007127428075"
     
     let cardNumberWhitespace        = "3760 3171  0126       369"
     
     // Invalid Card Numbers
     let cardNumberInvalid_1         = "0000000000000000"
     let cardNumberInvalid_2         = "67628 568"
+    
+    // Condition - Valid Card Number pairs
+    let cardConditionsAndNumbers: [(CreditCardType, CreditCardCondition, String)] = [
+        (CreditCardType.americanExpress,    CreditCardCondition(cardType: .americanExpress),        "376031710126369"),
+        (CreditCardType.dinersClub,         CreditCardCondition(cardType: .dinersClub),             "30085182354725"),
+        (CreditCardType.discover,           CreditCardCondition(cardType: .discover),               "6011359046468736"),
+        (CreditCardType.jcb,                CreditCardCondition(cardType: .jcb),                    "3535983484092395"),
+        (CreditCardType.maestro,            CreditCardCondition(cardType: .maestro),                "6762856858323942"),
+        (CreditCardType.mastercard,         CreditCardCondition(cardType: .mastercard),             "5480785928215247"),
+        (CreditCardType.visa,               CreditCardCondition(cardType: .visa),                   "4024007127428075"),
+    ]
     
     
     
@@ -50,21 +45,19 @@ final class CreditCardConditionTests: XCTestCase {
         let expectedResult = true
         
         // Test
-        AssertCondition(conditionAll, testInput: cardNumberAmericanExpress, expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberDinersClub,      expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberDiscover,        expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberJcb,             expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberMaestro,         expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberMastercard,      expectedResult: expectedResult)
-        AssertCondition(conditionAll, testInput: cardNumberVisa,            expectedResult: expectedResult)
+        for pair in cardConditionsAndNumbers {
+            AssertCondition(conditionAll, testInput: pair.2, expectedResult: expectedResult)
+        }
         
-        AssertCondition(conditionAmericanExpress,   testInput: cardNumberAmericanExpress,   expectedResult: expectedResult)
-        AssertCondition(conditionDinersClub,        testInput: cardNumberDinersClub,        expectedResult: expectedResult)
-        AssertCondition(conditionDiscover,          testInput: cardNumberDiscover,          expectedResult: expectedResult)
-        AssertCondition(conditionJcb,               testInput: cardNumberJcb,               expectedResult: expectedResult)
-        AssertCondition(conditionMaestro,           testInput: cardNumberMaestro,           expectedResult: expectedResult)
-        AssertCondition(conditionMasterCard,        testInput: cardNumberMastercard,        expectedResult: expectedResult)
-        AssertCondition(conditionVisa,              testInput: cardNumberVisa,              expectedResult: expectedResult)
+        for pair in cardConditionsAndNumbers {
+            AssertCondition(pair.1, testInput: pair.2, expectedResult: expectedResult)
+        }
+        
+        for card in cardConditionsAndNumbers {
+            for pair in cardConditionsAndNumbers {
+                AssertCondition(card.1, testInput: pair.2,   expectedResult: (pair.0 == card.0))
+            }
+        }
         
         AssertCondition(conditionAll, testInput: cardNumberWhitespace, expectedResult: expectedResult)
     }
@@ -79,75 +72,5 @@ final class CreditCardConditionTests: XCTestCase {
         // Test
         AssertCondition(conditionAll, testInput: cardNumberInvalid_1, expectedResult: expectedResult)
         AssertCondition(conditionAll, testInput: cardNumberInvalid_2, expectedResult: expectedResult)
-    }
-    
-    func testCreditCardCondition_AmericanExpress_Failure () {
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberAmericanExpress,   expectedResult: true)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionAmericanExpress, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_DinersClub_Failure () {
-        AssertCondition(conditionDinersClub, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionDinersClub, testInput: cardNumberDinersClub,        expectedResult: true)
-        AssertCondition(conditionDinersClub, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionDinersClub, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionDinersClub, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionDinersClub, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionDinersClub, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_Discover_Failure () {
-        AssertCondition(conditionDiscover, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionDiscover, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionDiscover, testInput: cardNumberDiscover,          expectedResult: true)
-        AssertCondition(conditionDiscover, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionDiscover, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionDiscover, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionDiscover, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_Jcb_Failure () {
-        AssertCondition(conditionJcb, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionJcb, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionJcb, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionJcb, testInput: cardNumberJcb,               expectedResult: true)
-        AssertCondition(conditionJcb, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionJcb, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionJcb, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_Maestro_Failure () {
-        AssertCondition(conditionMaestro, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionMaestro, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionMaestro, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionMaestro, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionMaestro, testInput: cardNumberMaestro,           expectedResult: true)
-        AssertCondition(conditionMaestro, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionMaestro, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_MasterCard_Failure () {
-        AssertCondition(conditionMasterCard, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionMasterCard, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionMasterCard, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionMasterCard, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionMasterCard, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionMasterCard, testInput: cardNumberMastercard,        expectedResult: true)
-        AssertCondition(conditionMasterCard, testInput: cardNumberVisa,              expectedResult: false)
-    }
-    
-    func testCreditCardCondition_Visa_Failure () {
-        AssertCondition(conditionVisa, testInput: cardNumberAmericanExpress,   expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberDinersClub,        expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberDiscover,          expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberJcb,               expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberMaestro,           expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberMastercard,        expectedResult: false)
-        AssertCondition(conditionVisa, testInput: cardNumberVisa,              expectedResult: true)
     }
 }
