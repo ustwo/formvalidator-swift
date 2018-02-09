7d1b2d0e237dc720d4d435284296a45c846a0cfa
Aaron McTavish <aamct@ustwo.com>
2018-02-09 08:09:37 +0000

    Migrate SwiftLint script to external file
    Resolves #50 `Migrate 'Run Script Phase' Scripts to External Files`

---
+++ Scripts/swiftlint_xcode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if which swiftlint >/dev/null; then
+  swiftlint
+else
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
